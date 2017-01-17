f708856b812036cab7a30b8a9310e003b7083a9d
Roger Taracha <rojtaracha@gmail.com>
2017-01-17 18:29:44 +0300

    (Another Test Case): A Test Case that looks up an entry in the phonebook by name.

--- phonebook.py
+++ phonebook.py
@@ -3,4 +3,15 @@
 class Phonebook:
     """Phonebook Class."""
 
-    pass
+    def __init__(self):
+        """The constructor for the Phonebook class."""
+        self.entries = {}
+
+    def add(self, name, number):
+        """Method that adds a name and number to the phonebook."""
+        self.entries[name] = number
+
+
+    def lookup(self, name):
+        """Method that looks up a name in the phonebook."""
+        return self.entries[name]
--- test_phonebook.py
+++ test_phonebook.py
@@ -10,3 +10,9 @@ class PhonebookTest(unittest.TestCase):
     def test_create_phonebook(self):
         """Test Case that creates the phonebook."""
         phonebook = Phonebook()
+
+    def test_lookup_entry_by_name(self):
+        """Test case that looks up an entry."""
+        phonebook = Phonebook()
+        phonebook.add("Bob", "12345")
+        self.assertEqual("12345", phonebook.lookup("Bob"))
