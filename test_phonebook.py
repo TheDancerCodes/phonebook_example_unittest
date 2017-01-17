32c9a5cec57ebde9d4a8c1396393c5ae6e45da39
Roger Taracha <rojtaracha@gmail.com>
2017-01-17 22:19:11 +0300

    [A Test Case using assertRaises]
    A KeyError is raised for a missing entry.

--- test_phonebook.py
+++ test_phonebook.py
@@ -12,7 +12,13 @@ class PhonebookTest(unittest.TestCase):
         phonebook = Phonebook()
 
     def test_lookup_entry_by_name(self):
-        """Test case that looks up an entry."""
+        """Test case that looks up an entry by name."""
         phonebook = Phonebook()
         phonebook.add("Bob", "12345")
         self.assertEqual("12345", phonebook.lookup("Bob"))
+
+    def test_missing_entry_raises_KeyError(self):
+        """Test case that raises KeyError for a missng entry."""
+        phonebook = Phonebook()
+        with self.assertRaises(KeyError):
+            phonebook.lookup("missing")
