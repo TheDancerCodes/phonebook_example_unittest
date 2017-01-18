392fe4836b4559c522ce19541904ae5774a1b4f8
Roger Taracha <rojtaracha@gmail.com>
2017-01-18 12:47:11 +0300

    (Using setUp)
      * This is a Test Fixture Method.
      * We used the setUp() to refactor our code and remove duplication of the Phonebook class instantiation code in all the methods.

--- test_phonebook.py
+++ test_phonebook.py
@@ -7,24 +7,21 @@ from phonebook import Phonebook
 class PhonebookTest(unittest.TestCase):
     """Class that contains the test cases."""
 
-    def test_create_phonebook(self):
-        """Test Case that creates the phonebook."""
-        phonebook = Phonebook()
+    def setUp(self):
+        """A Test Fixture method that sets up your testing evironment."""
+        self.phonebook = Phonebook()
 
     def test_lookup_entry_by_name(self):
         """Test case that looks up an entry by name."""
-        phonebook = Phonebook()
-        phonebook.add("Bob", "12345")
-        self.assertEqual("12345", phonebook.lookup("Bob"))
+        self.phonebook.add("Bob", "12345")
+        self.assertEqual("12345", self.phonebook.lookup("Bob"))
 
     def test_missing_entry_raises_KeyError(self):
         """Test case that raises KeyError for a missng entry."""
-        phonebook = Phonebook()
         with self.assertRaises(KeyError):
-            phonebook.lookup("missing")
+            self.phonebook.lookup("missing")
 
     @unittest.skip("WIP")
     def test_empty_phonebook_is_consistent(self):
         """Test Case that check that empty phonebook is consistent."""
-        phonebook = Phonebook()
-        self.assertTrue(phonebook.is_consistent())
+        self.assertTrue(self.phonebook.is_consistent())
