490df09d908e7deb4e1f383b1f2f9c6397a7b8b8
Roger Taracha <rojtaracha@gmail.com>
2017-01-19 14:57:30 +0300

    (Interpreting Failure Information)   * Added the Phonebook class and made the methods pass to get Assertion Errors.

---
+++ pytest/phonebook.py
@@ -0,0 +1,12 @@
+"""This file contains the Phonebook functionality."""
+
+class Phonebook():
+    """Phonebook Class."""
+
+    def add(self, name, number):
+        """Method that adds a name and number to the phonebook."""
+        pass
+
+    def lookup(self, name):
+        """Method that looks up a name in the phonebook."""
+        pass
--- pytest/test_phonebook.py
+++ pytest/test_phonebook.py
@@ -1,5 +1,9 @@
+"""This file contains test cases for the phonebook app."""
+
+from phonebook import Phonebook
 
 def test_add_and_lookp_entry():
+    """Test case that adds entry to phonebook & looks up an entry by name."""
     phonebook = Phonebook()
     phonebook.add("Bob", "123")
     assert "123" == phonebook.lookup("Bob")
